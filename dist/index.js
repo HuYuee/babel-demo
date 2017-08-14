8ffbde7d2d9c0264eb1b4a5bed249e03b6de629e
huyueb <1062887235@qq.com>
2017-08-14 21:45:03 +0800

    babel-preset-env：下一代最简便的转码插件，比lastest和es2015好用。因为可以配置target

--- dist/index.js
+++ dist/index.js
@@ -5,3 +5,7 @@ var x = function x(a) {
 };
 
 console.log(x(2));
+
+var obj = { a: 1 };
+var copy = Object.assign({}, obj);
+console.log(copy); // { a: 1 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 var x = a => a + 1;
 
 console.log(x(2));
+
+var obj = { a: 1 };
+var copy = Object.assign({}, obj);
+console.log(copy); // { a: 1 }
