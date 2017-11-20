0dc0c2f2e0fc8310e10d1dbf54574fc0484049aa
huyueb <1062887235@qq.com>
2017-11-20 15:18:24 +0800

    增加转义react

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-// require("http://design.yonyoucloud.com/static/babel-polyfill/6.20.0/polyfill.min.js");
+require('babel-polyfill');
 
 var x = function x(a) {
-  return a + 1;
+    return a + 1;
 };
 
 console.log(x(2));
@@ -11,3 +11,14 @@ console.log('abc'.padStart(10));
 var obj = { a: 1 };
 var copy = Object.assign({}, obj);
 console.log(copy); // { a: 1 }
+
+console.log('<h1>hello</h1>');
+var arr = [React.createElement(
+    'h1',
+    null,
+    'Hello world!'
+), React.createElement(
+    'h2',
+    null,
+    'React is awesome'
+)];
