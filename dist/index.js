21a1dff5cc61a47f3df00928ec3d10ff5eef0f99
huyueb <1062887235@qq.com>
2017-11-21 15:12:47 +0800

    增加转义react

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,99 @@
-'use strict';
+"use strict";
 
-require('babel-polyfill');
+var _createClass = (function() {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+  return function(Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+})();
 
-var x = function x(a) {
-    return a + 1;
-};
+function _possibleConstructorReturn(self, call) {
+  if (!self) {
+    throw new ReferenceError(
+      "this hasn't been initialised - super() hasn't been called"
+    );
+  }
+  return call && (typeof call === "object" || typeof call === "function")
+    ? call
+    : self;
+}
 
-console.log(x(2));
-console.log('abc'.padStart(10));
-var obj = { a: 1 };
-var copy = Object.assign({}, obj);
-console.log(copy); // { a: 1 }
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError(
+      "Super expression must either be null or a function, not " +
+        typeof superClass
+    );
+  }
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass)
+    Object.setPrototypeOf
+      ? Object.setPrototypeOf(subClass, superClass)
+      : (subClass.__proto__ = superClass);
+}
 
-console.log('<h1>hello</h1>');
-var arr = [React.createElement(
-    'h1',
-    null,
-    'Hello world!'
-), React.createElement(
-    'h2',
-    null,
-    'React is awesome'
-)];
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+var Animal = (function() {
+  function Animal() {
+    _classCallCheck(this, Animal);
+
+    this.type = "animal";
+  }
+
+  _createClass(Animal, [
+    {
+      key: "says",
+      value: function says(say) {
+        console.log(this.type + " says " + say);
+      }
+    }
+  ]);
+
+  return Animal;
+})();
+
+var animal = new Animal();
+animal.says("hello"); //animal says hello
+
+var Cat = (function(_Animal) {
+  _inherits(Cat, _Animal);
+
+  function Cat() {
+    _classCallCheck(this, Cat);
+
+    var _this = _possibleConstructorReturn(
+      this,
+      (Cat.__proto__ || Object.getPrototypeOf(Cat)).call(this)
+    );
+
+    _this.type = "cat";
+    return _this;
+  }
+
+  return Cat;
+})(Animal);
+
+var cat = new Cat();
+cat.says("hello"); //cat says hello
--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ import  "babel-polyfill";
 
 var x = a => a + 1;
 
-console.log(x(2));
-console.log('abc'.padStart(10));
-var obj = { a: 1 };
-var copy = Object.assign({}, obj);
-console.log(copy); // { a: 1 }
-
-console.log('<h1>hello</h1>')
 var arr = [
-    <h1>Hello world!</h1>,
-    <h2>React is awesome</h2>,
-   ];
+ <h1>Hello world!</h1>,
+ <h2>React is awesome</h2>,
+];
